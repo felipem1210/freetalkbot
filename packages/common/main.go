d4085e50475c907a16c581bbe7e71f43a2547f0e
felipem1210 <50753325+felipem1210@users.noreply.github.com>
2024-08-11 12:14:57 +0200

    feat: improves logging of the servers (#10)

--- packages/common/main.go
+++ packages/common/main.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -27,7 +29,39 @@ func ExecuteCommand(cmdString string) error {
 
 	err := cmd.Run()
 	if err != nil {
-		return fmt.Errorf("error executing command: %v", stderr.String())
+		return fmt.Errorf(stderr.String())
 	}
 	return nil
+}
+
+func SetLogger(ll string) {
+	logLvl := new(slog.LevelVar)
+	if ll == "" {
+		logLvl.Set(slog.LevelInfo)
+	}
+	if ll == "DEBUG" {
+		logLvl.Set(slog.LevelDebug)
+	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:       logLvl,
+		ReplaceAttr: replaceAttr,
+	}))
+	slog.SetDefault(logger)
+}
+
+func replaceAttr(_ []string, a slog.Attr) slog.Attr {
+	switch a.Value.Kind() {
+	case slog.KindAny:
+		switch v := a.Value.Any().(type) {
+		case error:
+			a.Value = fmtErr(v)
+		}
+	}
+	return a
+}
+
+func fmtErr(err error) slog.Value {
+	var groupValues []slog.Attr
+	groupValues = append(groupValues, slog.String("msg", err.Error()))
+	return slog.GroupValue(groupValues...)
 }
