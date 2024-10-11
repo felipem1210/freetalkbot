f22fb3086a286465427952d620fd9e8e08cec570
felipem1210 <felipem1210@gmail.com>
2024-10-11 11:37:40 +0200

    deletes unnecessary code

--- packages/assistants/main.go
+++ packages/assistants/main.go
@@ -3,9 +3,7 @@ package assistants
 import (
 	"os"
 
-	"github.com/felipem1210/freetalkbot/packages/anthropic"
 	"github.com/felipem1210/freetalkbot/packages/common"
-	"github.com/felipem1210/freetalkbot/packages/rasa"
 )
 
 func HandleAssistant(language string, sender string, message string) (common.Responses, error) {
@@ -13,14 +11,14 @@ func HandleAssistant(language string, sender string, message string) (common.Res
 	var err error
 	switch os.Getenv("ASSISTANT_TOOL") {
 	case "anthropic":
-		anthropicHandler := anthropic.Anthropic{}
+		anthropicHandler := Anthropic{}
 		response, err = anthropicHandler.Interact(sender, message)
 		if err != nil {
 			return nil, err
 		}
 
 	case "rasa":
-		rasaHandler := rasa.Rasa{
+		rasaHandler := Rasa{
 			MessageLanguage: language,
 			RasaLanguage:    os.Getenv("ASSISTANT_LANGUAGE"),
 		}
